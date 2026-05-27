fc27dea44056ff59f56a4bc75e9caea7bdd39117
Juan <juancardenas083@gmail.com>
2026-05-26 21:25:01 -0500

    Inputs y calendario

--- lib/form.dart
+++ lib/form.dart
@@ -5,10 +5,7 @@ class Wiget1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Formulario(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Formulario());
   }
 }
 
@@ -22,7 +19,16 @@ class Formulario extends StatefulWidget {
 class _FormularioState extends State<Formulario> {
   final _formKey = GlobalKey<FormState>();
 
+  //Controladores
   final nombreController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final passController = TextEditingController();
+
+  //Variables
+  String rol = '...';
+  DateTime? fecha;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,28 +38,125 @@ class _FormularioState extends State<Formulario> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
-        
+
         child: Form(
           key: _formKey,
           child: Column(
             children: [
+              const Icon(
+                Icons.account_circle,
+                size: 60,
+                color: Color.fromARGB(255, 255, 145, 145),
+              ),
               const Text(
                 'Registro user',
                 style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+
+              TextFormField(
+                controller: nombreController,
+
+                decoration: InputDecoration(
+                  labelText: "Nombre user",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
                 ),
-                const SizedBox(height: 30,),
+              ),
 
-                TextFormField(
-                  controller: nombreController,
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
 
-                  decoration: InputDecoration(
-                    labelText: "Nombre user",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person)
+                decoration: const InputDecoration(
+                  labelText: 'Correo',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: phoneController,
+                keyboardType: TextInputType.phone,
+
+                decoration: const InputDecoration(
+                  labelText: 'Telefono',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.phone),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: passController,
+                obscureText: true,
+
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+              DropdownButtonFormField(
+                value: rol,
+
+                decoration: const InputDecoration(
+                  labelText: 'Rol',
+                  border: OutlineInputBorder(),
+                ),
+
+                items: const [
+                  DropdownMenuItem(value: '...', child: Text('...')),
+                  DropdownMenuItem(value: 'Usuario', child: Text('Usuario')),
+                  DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                  DropdownMenuItem(value: 'Cliente', child: Text('Cliente')),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    rol = value!;
+                  });
+                },
+              ),
+
+              const SizedBox(height: 20),
+
+              //Contenedor vacio sizedbox
+              SizedBox(
+                //Tomar el ancho que necesite, no demas
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                  icon: Icon(Icons.date_range),
+                  label: Text(
+                    fecha == null
+                        ? 'Seleccionar fecha'
+                        : 'Fecha: ${fecha!.day}/${fecha!.month}/${fecha!.year}',
                   ),
-                )
+
+                  onPressed: () async {
+                    DateTime? pickdate = await showDatePicker(
+                      context: context,
+                      //F inicial
+                      initialDate: DateTime.now(),
+                      //F maxima hacia atras
+                      firstDate: DateTime(1950),
+                      //F maxima hacia adelante
+                      lastDate: DateTime(2027),
+                    );
+
+                    if (fecha != null) {
+                      setState(() {
+                        fecha = pickdate;
+                      });
+                    }
+                  },
+                ),
+              ),
             ],
-          )
+          ),
         ),
       ),
     );
